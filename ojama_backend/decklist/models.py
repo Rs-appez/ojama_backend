5a2236eb4df09edb43192c2a0eadd9e2a6995c13
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-04-30 23:13:15 +0200

    Update DecklistUrl model to remove unique constraint on name field

--- ojama_backend/decklist/models.py
+++ ojama_backend/decklist/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class DecklistUrl(models.Model):
     url = models.CharField(max_length=200)
-    name = models.CharField(max_length=200,unique=True)
+    name = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     post_by = models.CharField(max_length=200)
 
