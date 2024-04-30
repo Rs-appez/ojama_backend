e15b8c959a393bf42b26cb3a44dbfd2ad585ad8b
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-04-30 21:57:59 +0200

    Update DecklistUrl model with additional fields

--- ojama_backend/decklist/models.py
+++ ojama_backend/decklist/models.py
@@ -3,5 +3,9 @@ from django.db import models
 # Create your models here.
 class DecklistUrl(models.Model):
     url = models.CharField(max_length=200)
+    name = models.CharField(max_length=200,unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    post_by = models.CharField(max_length=200)
+
     def __str__(self):
-        return self.url
+        return f"{self.name} : {self.url}"
