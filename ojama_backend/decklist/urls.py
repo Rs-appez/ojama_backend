c40dc059cb74a579b4771718c3cfaf6f4e2f7fcc
Rs-appez <e8cca13e-ffd8-4656-a62d-dd311b0215a3@alias.appez.cafe>
2024-08-21 17:40:51 +0200

    add route banlist

--- ojama_backend/decklist/urls.py
+++ ojama_backend/decklist/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'decklisturls', views.DecklistUrlViewSet, basename='decklisturl')
+router.register(r'banlist', views.BanlistViewSet, basename='banlist')
 
 urlpatterns = [
     path('', include(router.urls)),
